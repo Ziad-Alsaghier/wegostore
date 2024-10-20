5a222f41d2b30b6cc395132ca9f550fda422d0b4
Ziad <wegostationdev@gmail.com>
2024-10-20 12:57:32 +0200

    Sign Up Module

--- app/Http/Controllers/api/v1/user/SignUpController.php
+++ app/Http/Controllers/api/v1/user/SignUpController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\api\v1\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SignUpController extends Controller
 {
-    // This Is About User SignUp
+    // This Controller About Signup User
+
+    public function __construct(private User $user){}
 
     
 }
