7343ca89330717fbf579809e8d6bff3444c6104b
Ziad <wegostationdev@gmail.com>
2024-10-26 10:37:17 +0200

    Edit Middleware User

--- routes/user.php
+++ routes/user.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\v1\user\SignUpController ;
 
 Route::prefix('/v1')->group(function () {
-    Route::withoutMiddleware('IsUser')->group(function () { // This All Route out Of Middleware User 
+    Route::withoutMiddleware(['IsUser','auth:sanctum'])->group(function () { // This All Route out Of Middleware User
         Route::controller(SignUpController::class)->group(function (){ // Sign Up Routes
                 Route::post(uri:'signUp', action:"signUp"); // POST /sign1Up  
         });
