88e15a0d03bcc64ec38c6e7fb5c00621da9dd1b2
Ahmed <ahmedahmadahmid73@gmail.com>
2024-11-06 09:28:09 +0200

    make payment history

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+use App\Models\Plan;
+use App\Models\Payment;
+use App\Models\User;
+use App\Models\Domain;
+use App\Models\Extra;
+
 class Order extends Model
 {
     protected $fillable = [
@@ -27,6 +33,14 @@ class Order extends Model
 
     public function users():BelongsTo{
       return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function domain():BelongsTo{
+      return $this->belongsTo(Domain::class,'domain_id');
+    }
+
+    public function extra():BelongsTo{
+      return $this->belongsTo(Extra::class,'extra_id');
     }
      
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+use App\Models\Order;
+
 class Payment extends Model
 {
     protected $fillable = [
@@ -17,15 +19,17 @@ class Payment extends Model
         'invoice_image',
         'status'
     ];
+
     public function payment_method():BelongsTo{ // This Relation One To Many PaymentMethod has Many Payments
         return $this->belongsTo(PaymentMethod::class,'payment_method_id');
     }
 
     public function user(){
         return $this->belongsTo(User::class, 'user_id');
-    }
-    public function plan():BelongsTo{
-        return $this->belongsTo(Plan::class,'plan_id');
+    } 
+
+    public function orders():HasMany{
+        return $this->hasMany(Order::class,'payment_id');
     }
     //
 }
