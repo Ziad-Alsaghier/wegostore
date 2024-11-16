7059ce11412e2e9855391fa486b92c80780a177e
Ahmed <ahmedahmadahmid73@gmail.com>
2024-11-16 13:12:07 +0200

    commit

--- resources/views/emails/Signup.blade.php
+++ resources/views/emails/Signup.blade.php
@@ -18,31 +18,9 @@
             <td style="padding: 10px; border: 1px solid #ddd;">{{$user->phone}}</td>
             <td style="padding: 10px; border: 1px solid #ddd;">رقم الهاتف</td>
         </tr>
-        @if ($user->role == 'student')
         <tr>
             <td style="padding: 10px; border: 1px solid #ddd;">{{$user->gender }}</td>
             <td style="padding: 10px; border: 1px solid #ddd;">نوع</td>
         </tr>
-        <tr>
-            <td style="padding: 10px; border: 1px solid #ddd;">{{$user->parent_phone}}</td>
-            <td style="padding: 10px; border: 1px solid #ddd;">رقم ولى الأمر</td>
-        </tr>
-        <tr>
-            <td style="padding: 10px; border: 1px solid #ddd;">{{$user->category}}</td>
-            <td style="padding: 10px; border: 1px solid #ddd;">الصف</td>
-        </tr>
-        <tr>
-            <td style="padding: 10px; border: 1px solid #ddd;">{{$user->parent}}</td>
-            <td style="padding: 10px; border: 1px solid #ddd;">أسم ولى الأمر</td>
-        </tr>
-        <tr>
-            <td style="padding: 10px; border: 1px solid #ddd;">{{$user->education}}</td>
-            <td style="padding: 10px; border: 1px solid #ddd;">نوع التعليم</td>
-        </tr>
-        <tr>
-            <td style="padding: 10px; border: 1px solid #ddd;">{{$user->job}}</td>
-            <td style="padding: 10px; border: 1px solid #ddd;">الوظيفة</td>
-        </tr>
-        @endif
     </tbody>
 </table>
