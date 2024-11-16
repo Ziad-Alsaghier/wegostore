c7ce8fb4d24c9ab2169dbd95789bbac005d9b3cf
Ahmed <ahmedahmadahmid73@gmail.com>
2024-11-16 10:56:51 +0200

    commit

--- resources/views/emails/PaymentInvoice.blade.php
+++ resources/views/emails/PaymentInvoice.blade.php
@@ -180,30 +180,56 @@
         <img src="{{url('storage/email/logo.png')}}" alt="wegoStore">
       </div>
     <div class="sectiondiv">
-        <div>
+        {{-- <div>
             <!-- Order Details -->
             <div class="section">
                 <h2>Order Details:</h2>
                 <p class="font-semibold">Order Number: #12345</p>
-                <p class="font-semibold">Order Date: {{$data['created_at']}}</p>
+                <p class="font-semibold">Order Date: </p>
             </div>
             <!-- Customer Information -->
             <div class="section">
                 <h2>Customer Information:</h2>
-                <p class="font-semibold">{{$data['created_at']}}</p>
-                <p class="font-semibold">{{$data['user']['email']}}</p>
-                <p class="font-semibold">{{$data['user']['phone']}}</p>
+                <p class="font-semibold"></p>
+                <p class="font-semibold"></p>
+                <p class="font-semibold"></p>
             </div>
         </div>
         <!-- Payment Details -->
         <div class="section" style="padding: 12px;padding-top: 0;">
             <h2>Payment Details:</h2>
             <div style="padding: 12px;background-color: #1A237E;border-radius: 2%;">
-                <p class="font-semibold" style="color: #fff;">{{$data['payment_method']['name']}}</p>
-                <p class="font-semibold" style="color: #fff;">{{$data['description']}}</p>
+                <p class="font-semibold" style="color: #fff;"></p>
+                <p class="font-semibold" style="color: #fff;"></p>
                 <p class="font-semibold" style="color: #fff;">123-456-7890</p>
             </div>
-        </div>
+        </div> --}}
+        <div>
+          <div>
+              <!-- Order Details -->
+              <div class="section">
+                  <h2>Order Details:</h2>
+                  <p class="font-semibold">Order Number: #12345</p>
+                  <p class="font-semibold">Order Date: {{$data['created_at']}}</p>
+              </div>
+              <!-- Customer Information -->
+              <div class="section">
+                  <h2>Customer Information:</h2>
+                  <p class="font-semibold">{{$data['created_at']}}</p>
+                  <p class="font-semibold">{{$data['user']['email']}}</p>
+                  <p class="font-semibold">{{$data['user']['phone']}}</p>
+              </div>
+          </div>
+          <!-- Payment Details -->
+          <div class="section">
+              <h2>Payment Details:</h2>
+              <div>
+                  <p class="font-semibold">{{$data['payment_method']['name']}}</p>
+                  <p class="font-semibold">{{$data['description']}}</p>
+                  <p class="font-semibold">123-456-7890</p>
+              </div>
+          </div>
+  
     </div>
 
       <!-- Divider -->
