fd511633bfac1d51ae2fe632692f0ce59eb3e459
Ahmed <Wegostationdev@gmail.com>
2024-12-09 10:09:26 +0200

    commit

--- app/Http/Controllers/api/v1/admin/payment/PaymentController.php
+++ app/Http/Controllers/api/v1/admin/payment/PaymentController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\api\v1\admin\payment\ApprovePayment;
 use App\Http\Requests\api\v1\admin\payment\ApprovePaymentRequest;
 use App\Models\Payment;
 use App\Models\Store;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
@@ -18,7 +19,8 @@ class PaymentController extends Controller
     // This About All Payment 
         public function __construct(
             private Payment $payment,
-            private Store $store
+            private Store $store,
+            private User $user,
         ){}
     public function bindPayment(){
         url:http://localhost/wegostore/public/admin/v1/payment/show/pending
@@ -65,7 +67,9 @@ class PaymentController extends Controller
         
         foreach ($payment->orders as $order) {
             if (!empty($order->plan_id)) {
-                $user = $payment->user_id;
+                $user = $this->user
+                ->where('id', $payment->user_id)
+                ->first();
                 $user->plan_id = $order->plan_id;
                 $duration = 1;
                 if ($order->price_cycle == 'yearly') {
